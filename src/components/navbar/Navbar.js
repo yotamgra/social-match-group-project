f5125bd37ba3af9a3051b595aee92e03af830b69
dimakuznetsow <108948435+dimakuznetsow@users.noreply.github.com>
2022-11-17 16:32:08 +0200

    newpost navbar added

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,21 @@
+import { AppBar, Avatar, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Navbar = () => {
+  return (
+    <AppBar position="static" color="warning">
+      <StyledToolbar>
+        <Typography variant="h4">Social Match</Typography>
+        <Avatar></Avatar>
+      </StyledToolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
