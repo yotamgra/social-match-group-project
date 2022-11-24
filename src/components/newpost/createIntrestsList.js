ff1e1799d1297db39156119247967d7bd38beeab
Yotam Granada <yotam.granada@gmail.com>
2022-11-24 16:01:14 +0200

    change autocomplete intrest to have one option only

--- src/components/newpost/createIntrestsList.js
+++
@@ -1,8 +0,0 @@
-fetch("./intrestsList.txt",{headers: {
-    'Content-Type': 'text/plain'
-  }
-})
-  .then((res) => res.text())
-  .then((data) => {
-const intrestsList = data.split(/\r?\n/);
-  });
