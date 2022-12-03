6d00d745518e1611250de3b22386978ac83a52f8
dimakuznetsow <108948435+dimakuznetsow@users.noreply.github.com>
2022-12-03 18:28:23 +0200

    spots, level fixed

--- src/components/allPosts/PostChips.js
+++ src/components/allPosts/PostChips.js
@@ -1,28 +1,27 @@
-import { Box, Chip } from '@mui/material'
-import React from 'react'
+import { Box, Chip } from "@mui/material";
+import React from "react";
 
-function PostChips({post}) {
+function PostChips({ post }) {
   return (
-    <Box sx={{ display: "flex", alignItems: "center", mr:"10px" }}>
-
-                  <Chip
-                    variant="outlined"
-                    label={`spots: 2  / ${post.spots}  `}
-                    // sx={{ mr: 1, float: "right" }}
-                  /> 
-                  <Chip
-                    variant="outlined"
-                    label={post.level}
-                    // sx={{ mr: 1, float: "right" }}
-                  />
-                  <Chip
-                    color="warning"
-                    variant="outlined"
-                    label={post.interest}
-                    // sx={{ float: "right" }}
-                  />
-                </Box>
-  )
+    <Box sx={{ display: "flex", alignItems: "center", mr: "10px" }}>
+      <Chip
+        variant="outlined"
+        label={`${post.spots - post.participants.length}  / ${post.spots}  `}
+        // sx={{ mr: 1, float: "right" }}
+      />
+      <Chip
+        variant="outlined"
+        label={post.level}
+        // sx={{ mr: 1, float: "right" }}
+      />
+      <Chip
+        color="warning"
+        variant="outlined"
+        label={post.interest}
+        // sx={{ float: "right" }}
+      />
+    </Box>
+  );
 }
 
-export default PostChips
+export default PostChips;
