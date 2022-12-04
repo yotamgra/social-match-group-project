f34378968b7d2f2062e42797ebb97c89aa754956
dimakuznetsow <108948435+dimakuznetsow@users.noreply.github.com>
2022-12-04 13:53:05 +0200

    dashboard styled

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -6,6 +6,8 @@ import NewPost from "../newpost/NewPost";
 import AllPosts from "../allPosts/AllPosts";
 import Filter from "../filter/Filter";
 import { usePosts } from "../../contexts/PostsContext";
+import { Container } from "@mui/system";
+import { Card, CardContent, TextField } from "@mui/material";
 
 const Dashboard = () => {
   const { logout } = useAuth();
@@ -25,15 +27,51 @@ const Dashboard = () => {
   return (
     <>
       <Navbar />
-      <button
-        onClick={() => {
-          setEditor(false);
-          navigate("/new-post");
-        }}
-      >
-        Create New Post
-      </button>
-      <Filter />
+      {/* <Filter /> */}
+      <Container maxWidth="sm">
+        <Card
+          sx={{
+            mt: 3,
+            mb: 5,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <CardContent>
+            <TextField
+              required
+              onFocus={() => {
+                setEditor(false);
+                navigate("/new-post");
+              }}
+              name="title"
+              placeholder="Title"
+              color="warning"
+              id="input-title"
+              variant="outlined"
+              fullWidth
+            />
+          </CardContent>
+          <CardContent>
+            <TextField
+              required
+              onFocus={() => {
+                setEditor(false);
+                navigate("/new-post");
+              }}
+              name="description"
+              placeholder="New Post"
+              color="warning"
+              id="input-newpost"
+              variant="outlined"
+              fullWidth
+              rows={5}
+              multiline
+            />
+          </CardContent>
+        </Card>
+      </Container>
       <AllPosts />
     </>
   );
